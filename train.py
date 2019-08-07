b3f8e7cfd927eced55aaa4005115d1f0050c3e97
DeepBlackSky <DeepBlackSky>
2019-08-07 21:30:51 +0900

    update

--- train.py
+++ train.py
@@ -4,17 +4,18 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import backend as K
 from tensorflow.keras.losses import mse
+import IPython
 
 def kl_loss(dummy, concated_param):
     z_mean, z_log_var = tf.split(concated_param, num_or_size_splits=2, axis=1)
     kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
     kl_loss = K.sum(kl_loss, axis=-1)
     kl_loss *= -0.5
-    return kl_loss
+    return K.mean(kl_loss) * 1000.0
 
 def denoise_loss(y_true, y_pred):
     denoise_loss = mse(y_true, y_pred)
-    return denoise_loss
+    return denoise_loss * 100.0
 
 def loss(y_true, y_pred, z_mean, z_log_var):
     kl = kl_loss(z_mean, z_log_var)
@@ -22,14 +23,14 @@ def loss(y_true, y_pred, z_mean, z_log_var):
     loss = K.mean(denoise + kl)
     return loss
 
-def train(iteration, epoch, num_sounds):
+def train(iteration, epoch, num_sounds, is_jupyter):
     model = DenoiserModel()
     vd = model.build_model(512)
     data = Data('cv-valid-dev.csv')
 
     vd.compile(optimizer='adam',
                loss=[kl_loss, denoise_loss],
-               loss_weights=[10.0, 1.0])
+               loss_weights=[1.0, 1.0])
 
     for i in range(iteration):
         ys = data.make_batch(num_sounds)
@@ -41,5 +42,15 @@ def train(iteration, epoch, num_sounds):
                epochs=epoch,
                validation_split=0.3)
 
+        if is_jupyter:
+            denoised = denoise(vd, xs[0:100])
+            IPython.display.display(IPython.display.Audio(data=denoised, rate=48000))
+
+def denoise(model, data):
+    denoised = model.predict(data)
+    denoised = denoised[1]
+    denoised = np.array(denoised).flatten()
+    return denoised
+
 if __name__ == '__main__':
-    train(50, 5, 1)
+    train(50, 1, 1, False)
